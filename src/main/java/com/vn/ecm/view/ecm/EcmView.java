abcd708f16718cf8fd168587e9d9a67302d8f683
luudong <luuvandong2004@gmail.com>
2025-10-06 11:27:12 +0700

    update upload file

--- src/main/java/com/vn/ecm/view/ecm/EcmView.java
+++ src/main/java/com/vn/ecm/view/ecm/EcmView.java
@@ -1,22 +1,31 @@
 package com.vn.ecm.view.ecm;
 
-
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.grid.ItemClickEvent;
-import com.vaadin.flow.data.selection.SelectionEvent;
+
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.upload.SucceededEvent;
+
+import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.router.Route;
 import com.vn.ecm.entity.File;
 import com.vn.ecm.entity.Folder;
 import com.vn.ecm.view.main.MainView;
+import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.component.grid.TreeDataGrid;
-import io.jmix.flowui.data.grid.ContainerDataGridItems;
+import io.jmix.flowui.component.upload.receiver.MultiFileTemporaryStorageBuffer;
+import io.jmix.flowui.component.upload.receiver.TemporaryStorageFileData;
 import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.CollectionLoader;
+import io.jmix.flowui.upload.TemporaryStorage;
 import io.jmix.flowui.view.*;
-import jakarta.persistence.criteria.Selection;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Collection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.UUID;
 
 @Route(value = "ECM-view", layout = MainView.class)
 @ViewController(id = "EcmView")
@@ -34,6 +43,12 @@ public class EcmView extends StandardView {
     private DataGrid<File> fileDataGird;
     @ViewComponent
     private CollectionLoader<File> filesDl;
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private TemporaryStorage temporaryStorage;
+    private Upload upload;
+
 
     @Subscribe
     protected void onInit(InitEvent event) {
@@ -53,5 +68,19 @@ public class EcmView extends StandardView {
         filesDl.load();
     }
 
+    @Subscribe("upload")
+    public void onUploadSucceeded(final SucceededEvent event) {
+        StringBuilder sb = new StringBuilder("<div>Uploaded file(s):");
+        if (event.getUpload().getReceiver() instanceof MultiFileTemporaryStorageBuffer buffer) {
+        }
+    }
+
+    @Subscribe("upload")
+    public void onUploadFileRejected(final com.vaadin.flow.component.upload.FileRejectedEvent event) {
+        notifications.create(event.getErrorMessage())
+                .withThemeVariant(NotificationVariant.LUMO_WARNING)
+                .withDuration(5000)
+                .show();
+    }
 
 }
