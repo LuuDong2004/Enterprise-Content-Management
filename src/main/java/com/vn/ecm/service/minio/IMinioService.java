c1be6e19a2fd3ef0755d40699c207effa46ec650
Nguyen-DucAnh <anhnd05122004@gmail.com>
2025-10-08 09:53:57 +0700

    fix loi code ngu cua tk Dong

--- src/main/java/com/vn/ecm/service/minio/IMinioService.java
+++
@@ -1,9 +0,0 @@
-package com.vn.ecm.service.minio;
-
-import com.vn.ecm.entity.File;
-
-import java.io.InputStream;
-
-public interface IService {
-    String upload(String fileRef, String fileName, InputStream inputStream, String contentType);
-}
--- src/main/java/com/vn/ecm/service/minio/impl/MinioServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.vn.ecm.service.minio.impl;
-
-public class ServiceImpl {
-}
