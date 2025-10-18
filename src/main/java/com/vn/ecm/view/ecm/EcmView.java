cedf6b6a472101020f17aa407559485bb3c720f5
Nguyen-DucAnh <anhnd05122004@gmail.com>
2025-10-18 11:09:25 +0700

    fix vie

--- src/main/java/com/vn/ecm/view/ecm/EcmView.java
+++ src/main/java/com/vn/ecm/view/ecm/EcmView.java
@@ -198,7 +198,7 @@ public class EcmView extends StandardView implements BeforeEnterObserver, AfterN
         }
         //Add hierarchy column
         TreeDataGrid.Column<Folder> nameColumn = foldersTree.addComponentHierarchyColumn(item -> renderFolderItem(item));
-        nameColumn.setHeader("Name");
+        nameColumn.setHeader("Thư mục");
         nameColumn.setWidth("500px");
         //set position for hierarchy column
         foldersTree.setColumnPosition(nameColumn, 0);
