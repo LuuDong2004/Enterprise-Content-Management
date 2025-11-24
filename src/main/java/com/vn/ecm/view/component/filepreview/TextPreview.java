6ce3e75574863ec0838bb60ce262df7fdca1a8ed
luudong <luuvandong2004@gmail.com>
2025-11-24 11:47:08 +0700

    fix

--- src/main/java/com/vn/ecm/view/component/filepreview/TextPreview.java
+++ src/main/java/com/vn/ecm/view/component/filepreview/TextPreview.java
@@ -6,13 +6,8 @@ import com.vn.ecm.ecm.storage.DynamicStorageManager;
 import gr.netmechanics.jmix.tinymce.component.TinyMceEditor;
 import io.jmix.core.FileRef;
 import io.jmix.core.FileStorage;
-import io.jmix.core.FileStorageLocator;
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.view.*;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.InputStream;
@@ -25,8 +20,7 @@ public class TextPreview extends StandardView {
     @ViewComponent
     private TinyMceEditor textFormField;
     private FileRef inputFile;
-    @Autowired
-    private FileStorageLocator fileStorageLocator;
+
     @Autowired
     private DynamicStorageManager dynamicStorageManager;
     @Autowired
@@ -42,25 +36,11 @@ public class TextPreview extends StandardView {
             return;
         }
         String storageName = inputFile.getStorageName();
-        String fileName = inputFile.getFileName().toLowerCase();
         try {
             FileStorage storage = dynamicStorageManager.getFileStorageByName(storageName);
 
             try (InputStream is = storage.openStream(inputFile)) {
-                String resultText;
-                if (fileName.endsWith(".docx")) {
-                    try (XWPFDocument docx = new XWPFDocument(is);
-                         XWPFWordExtractor extractor = new XWPFWordExtractor(docx)) {
-                        resultText = extractor.getText();
-                    }
-                } else if (fileName.endsWith(".doc")) {
-                    try (HWPFDocument doc = new HWPFDocument(is);
-                         WordExtractor extractor = new WordExtractor(doc)) {
-                        resultText = extractor.getText();
-                    }
-                } else {
-                    resultText = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-                }
+                String resultText = new String(is.readAllBytes(), StandardCharsets.UTF_8);
                 textFormField.setValue(resultText);
                 textFormField.setWidthFull();
                 textFormField.setHeight("1000%");
